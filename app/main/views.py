57cb0e0beeb152e9c744eae5409ab0b8cc07df2a
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-06-25 23:23:02 +0200

    swap all <-> random

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,6 @@ from . import main
 def index():
     return render_template('index.html')
 
-@main.route('/random', methods=['GET'])
-def random():
-    return render_template('random.html')
+@main.route('/all', methods=['GET'])
+def all():
+    return render_template('all.html')
