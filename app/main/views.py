f99bc532b65b9edae635842ea8c022ba125fba02
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-06-25 09:04:32 +0200

    show random quote

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@main.route('/random', methods=['GET'])
+def random():
+    return render_template('random.html')
