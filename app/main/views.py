cdaca00ba355fc24187140aa0c1490662e65009e
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-06-26 13:42:40 +0200

    add kids

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,12 @@ from . import main
 
 @main.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('random.html')
 
 @main.route('/all', methods=['GET'])
 def all():
     return render_template('all.html')
+
+@main.route('/kids', methods=['GET'])
+def kids():
+    return render_template('kids.html')
